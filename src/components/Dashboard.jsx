586183bb47d2a968b9d4027bd94ccd01cbceff87
gretchelg <gretchelglopez@gmail.com>
2024-04-07 19:11:42 +0200

    added Charts

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,3 +1,15 @@
+import { useContext, useState, useEffect } from "react";
+import { useJwt } from "react-jwt";
+import { Link } from "react-router-dom";
+import { ThemeContext } from "../context/ThemeContext";
+import { DataContext } from "../context/DataContext";
+import { AuthContext } from "../context/AuthContext";
+import "./style/Dashboard.css"
+
+import { BorderStyle, ConstructionOutlined, FunctionsOutlined,} from "@mui/icons-material";
+import { MenuItem, InputLabel, FormControl, Select } from "@mui/material";
+import { Container, Box, Typography, LinearProgress } from "@mui/material";
+
 
 export default function Dashboard() {
     return (
